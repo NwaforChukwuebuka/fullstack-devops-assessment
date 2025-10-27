29366d2457b699d03c711cab872a76c86e144357
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2025-10-27 01:14:35 +0100

    Add detailed logging to exception handler for debugging

--- backend/app/Exceptions/Handler.php
+++ backend/app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -44,6 +45,28 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $exception)
+    {
+        // Log the full exception for debugging
+        \Log::error('Exception in ' . get_class($exception), [
+            'message' => $exception->getMessage(),
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine(),
+            'trace' => $exception->getTraceAsString(),
+        ]);
+
+        return parent::render($request, $exception);
     }
 }
 
